1509c7e61d4e732c111e0eef24a8ccc68d72dee0
1stVyacheslav <komaroff4@gmail.com>
2019-06-11 18:50:07 +0500

    style add-item panel lesson 41 (section 4)

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -4,15 +4,13 @@ import './add-item.css'
 
 const AddItem = () => {
 	return (
-		<form className='add-item form-inline'>
-			<div className='input-group-sm'>
+		
+			<div className='add-item d-flex'>
 				<input type='text' className='add-item-input form-control' placeholder='Add Item' />
-				<div className='input-group-append'>
-					<button type='button' className='btn btn-primary btn-sm'>Add Item</button>					
-				</div>
+				<button type='button' className='btn btn-primary'>Add Item</button>				
 			</div>
 
-		</form>
+		
 		
 	)
 };
